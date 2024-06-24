50339735a67c5fb576a85dba623abfeaffa1fef7
Adrian Scherbichler <adrian.scherbichler@gmail.com>
2024-06-24 11:08:44 +0200

    Changed page behaviour and added text resizing function

--- src/app.rs
+++ src/app.rs
@@ -1,107 +1,20 @@
-/*use serde::{Deserialize, Serialize};
-use serde_wasm_bindgen::to_value;
-use wasm_bindgen::prelude::*;
-use wasm_bindgen_futures::spawn_local;
 use yew::prelude::*;
-
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "tauri"])]
-    async fn invoke(cmd: &str, args: JsValue) -> JsValue;
-}
-
-#[derive(Serialize, Deserialize)]
-struct GreetArgs<'a> {
-    name: &'a str,
-}
-
-#[function_component(App)]
-pub fn app() -> Html {
-    let greet_input_ref = use_node_ref();
-
-    let name = use_state(|| String::new());
-
-    let greet_msg = use_state(|| String::new());
-    {
-        let greet_msg = greet_msg.clone();
-        let name = name.clone();
-        let name2 = name.clone();
-        use_effect_with(
-            name2,
-            move |_| {
-                spawn_local(async move {
-                    if name.is_empty() {
-                        return;
-                    }
-
-                    let args = to_value(&GreetArgs { name: &*name }).unwrap();
-                    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-                    let new_msg = invoke("greet", args).await.as_string().unwrap();
-                    greet_msg.set(new_msg);
-                });
-
-                || {}
-            },
-        );
-    }
-
-    let greet = {
-        let name = name.clone();
-        let greet_input_ref = greet_input_ref.clone();
-        Callback::from(move |e: SubmitEvent| {
-            e.prevent_default();
-            name.set(
-                greet_input_ref
-                    .cast::<web_sys::HtmlInputElement>()
-                    .unwrap()
-                    .value(),
-            );
-        })
-    };
-
-    html! {
-        <main class="container">
-            <div class="row">
-                <a href="https://tauri.app" target="_blank">
-                    <img src="public/tauri.svg" class="logo tauri" alt="Tauri logo"/>
-                </a>
-            </div>
-
-            <p>{"Click on the Tauri and Yew logos to learn more."}</p>
-
-            <p>
-                {"Recommended IDE setup: "}
-                <a href="https://code.visualstudio.com/" target="_blank">{"VS Code"}</a>
-                {" + "}
-                <a href="https://github.com/tauri-apps/tauri-vscode" target="_blank">{"Tauri"}</a>
-                {" + "}
-                <a href="https://github.com/rust-lang/rust-analyzer" target="_blank">{"rust-analyzer"}</a>
-            </p>
-
-            <form class="row" onsubmit={greet}>
-                <input id="greet-input" ref={greet_input_ref} placeholder="Enter a name..." />
-                <button type="submit">{"Greet"}</button>
-            </form>
-
-            <p><b>{ &*greet_msg }</b></p>
-        </main>
-    }
-}
-*/
-
-use yew::prelude::*;
-use web_sys::HtmlElement;
+use web_sys::{HtmlElement, HtmlInputElement};
 use yew::events::InputEvent;
+use wasm_bindgen::JsCast;
 
 #[function_component(App)]
 pub fn app() -> Html {
     let text_input_ref = use_node_ref();
     let lines = use_state(Vec::new);
+    let font_size = use_state(|| 16.0);
 
-    let on_input = {
-        let lines = lines.clone();
+
+    let on_text_input = {
         let text_input_ref = text_input_ref.clone();
-        Callback::from(move |_: InputEvent| {
+        let lines = lines.clone();
+
+        Callback::from(move |_| {
             if let Some(input) = text_input_ref.cast::<HtmlElement>() {
                 let inner_text = input.inner_text();
                 let new_lines: Vec<String> = inner_text.lines().map(String::from).collect();
@@ -110,9 +23,31 @@ pub fn app() -> Html {
         })
     };
 
+    let on_font_size_change = {
+        let font_size = font_size.clone();
+
+        Callback::from(move |e: InputEvent| {
+            if let Some(input) = e.target_dyn_into::<HtmlInputElement>() {
+                let new_font_size = input.value_as_number();
+                font_size.set(new_font_size);
+
+                if let Some(document) = web_sys::window().and_then(|w| w.document()) {
+                    if let Some(style) = document.get_element_by_id("dynamic-style").and_then(|el| el.dyn_into::<HtmlElement>().ok()) {
+                        style.set_inner_html(&format!(":root {{ --font-size: {}px; }}", new_font_size));
+                    }
+                }
+            }
+        })
+    };
+
     html! {
         <>
-            <div class="topbar">
+            <style id="dynamic-style"></style>
+            <div class="menubar">
+                <p>{"Placeholder"}</p>
+                <input type="number" value={format!("{}", *font_size)} oninput={on_font_size_change} />
+            </div>
+            <div class="toolbar">
                 <p>{"Placeholder"}</p>
             </div>
 
@@ -120,12 +55,14 @@ pub fn app() -> Html {
             </div>
 
             <div class="notepad-container">
-                <div
-                    class="notepad-textarea"
-                    ref={text_input_ref}
-                    contenteditable = "true"
-                    oninput={on_input}
-                ></div>
+                <div class="notepad-wrapper">
+                    <div
+                        class="notepad-textarea"
+                        ref={text_input_ref}
+                        contenteditable = "true"
+                        oninput={on_text_input}
+                    ></div>
+                </div>
             </div>
 
             <div class="bottombar">
@@ -134,3 +71,4 @@ pub fn app() -> Html {
         </>
     }
 }
+
