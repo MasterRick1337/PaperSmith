aa8979a3f4649d004883f6c569511fed3c0dd793
DotDo <jannis.ehrnhofer@gmx.at>
2024-10-04 10:34:06 +0200

    added write to json

--- src/statistics/session_time.rs
+++ src/statistics/session_time.rs
@@ -3,31 +3,69 @@ use chrono::TimeDelta;
 use yew::prelude::*;
 use yew_hooks::prelude::*;
 
+use serde_json::json;
+use std::path::Path;
+use wasm_bindgen_futures::spawn_local;
+use wasm_bindgen::prelude::wasm_bindgen;
+use wasm_bindgen::JsValue;
+use serde::{Serialize, Deserialize};
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "tauri"])]
+    async fn invoke(cmd: &str, args: JsValue) -> JsValue;
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct FileWriteData {
+    pub path: String,
+    pub content: String,
+}
+
 #[function_component]
 pub fn SessionTime() -> Html {
     let start_time = use_state(Local::now);
     let session_time = use_state(|| TimeDelta::new(0, 0).unwrap());
+    let formatted_time = use_state(|| String::from(""));
+    
+    {
+        let start_time = start_time.clone();
+        let session_time = session_time.clone();
+        let formatted_time = formatted_time.clone();
+        use_interval(
+        move || {
+                let start_time = start_time.clone();
+                let session_time = session_time.clone();
+                let formatted_time = formatted_time.clone();
+                spawn_local( async move {
+                    let current_time = Local::now();
+                    session_time.set(current_time - *start_time);
+                    
+                    let total_seconds = session_time.num_seconds();
+                    let hours = total_seconds / 3600;
+                    let minutes = (total_seconds % 3600) / 60;
+                    let seconds = total_seconds % 60;
 
-    use_interval(
-        {
-            let session_time = session_time.clone();
-            move || {
-                let current_time = Local::now();
-                session_time.set(current_time - *start_time);
-            }
+                    formatted_time.set( format!("{:02}:{:02}:{:02}", hours, minutes, seconds));
+
+                    let session_time_json = json!({
+                        "session_time": <std::string::String as Clone>::clone(&*formatted_time.clone())
+                    }).to_string();
+
+                    let path = Path::new("C:\\Users\\janni\\Desktop\\Schule\\Diplomarbeit\\Program\\statistic\\session_time.json");
+                    let fileWriteData = FileWriteData {path: path.to_string_lossy().to_string(), content: session_time_json};
+
+                    invoke("write_to_file", serde_wasm_bindgen::to_value(&fileWriteData).unwrap()).await;
+                })
         },
         1000,
     );
 
-    let total_seconds = session_time.num_seconds();
-    let hours = total_seconds / 3600;
-    let minutes = (total_seconds % 3600) / 60;
-    let seconds = total_seconds % 60;
-
-    let formatted_time = format!("{:02}:{:02}:{:02}", hours, minutes, seconds);
+    
+    }
 
     html! {
 
-        <p>{formatted_time}</p>
+        <p>{<std::string::String as Clone>::clone(&*formatted_time.clone())}</p>
     }
 }
--- src/statistics/word_count.rs
+++ src/statistics/word_count.rs
@@ -1,11 +1,30 @@
 use web_sys::HtmlElement;
 use yew::prelude::*;
 use yew_hooks::prelude::*;
+use serde_json::json;
+use std::path::Path;
+use wasm_bindgen_futures::spawn_local;
+
+use wasm_bindgen::prelude::wasm_bindgen;
+use wasm_bindgen::JsValue;
+use serde::{Serialize, Deserialize};
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "tauri"])]
+    async fn invoke(cmd: &str, args: JsValue) -> JsValue;
+}
 
 #[derive(Properties, PartialEq)]
     pub struct WordCountProps {
         pub pages_ref: NodeRef,
     }
+
+#[derive(Serialize, Deserialize)]
+pub struct FileWriteData {
+    pub path: String,
+    pub content: String,
+}
 
 #[function_component]
     pub fn WordCount(WordCountProps { pages_ref }: &WordCountProps) -> Html {
@@ -14,17 +33,27 @@ use yew_hooks::prelude::*;
             let pages_ref = pages_ref.clone();
             let word_count = word_count.clone();
             use_interval(
-                {
+                move || {
                     let pages_ref = pages_ref.clone();
                     let word_count = word_count.clone();
-                    move || {
+                    spawn_local( async move {
                         if let Some(pages_element) = pages_ref.cast::<HtmlElement>() {
                             let text = pages_element.inner_text();
                             let count = text.split_whitespace().count();
                             word_count.set(count);
+
+                            let word_count_json = json!({
+                                "word_count": count
+                            }).to_string();
+                            
+                            let path = Path::new("C:\\Users\\janni\\Desktop\\Schule\\Diplomarbeit\\Program\\statistic\\word_count.json");
+                            let fileWriteData = FileWriteData {path: path.to_string_lossy().to_string(), content: word_count_json};
+                            
+                            invoke("write_to_file", serde_wasm_bindgen::to_value(&fileWriteData).unwrap()).await;
                         }
-                    }
+                    })
                 },
+                
                 1500,
             )
         }
@@ -32,4 +61,4 @@ use yew_hooks::prelude::*;
         html! {
         <div>{format!("{} Words", *word_count)}</div>
     }
-    }
+}
