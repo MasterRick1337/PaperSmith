3e3ad686af996d1d0604030671f35b094e0b86fa
Toll <toll25@proton.me>
2024-10-20 23:37:17 +0200

    added validation to project creation

--- src/modal-system/modal.rs
+++ src/modal-system/modal.rs
@@ -13,7 +13,7 @@ pub fn modal(Props { content }: &Props) -> Html {
             <div
                 class="absolute top-0 left-0 z-50 bg-black/60 h-full w-full flex items-center justify-center text-text"
             >
-                <div class="bg-base rounded-lg max-w-[60%] min-w-[50%] p-8">
+                <div class="bg-base rounded-lg max-w-[80%] min-w-[80%] p-8">
                     { content.clone() }
                 </div>
             </div>
--- src/project-wizard/wizard.rs
+++ src/project-wizard/wizard.rs
@@ -1,8 +1,11 @@
+use std::path::{Path, PathBuf};
+
 use serde::Serialize;
 use serde_wasm_bindgen::to_value;
 use wasm_bindgen::prelude::wasm_bindgen;
 use wasm_bindgen::JsValue;
 use wasm_bindgen_futures::spawn_local;
+use web_sys::{HtmlButtonElement, HtmlElement, HtmlInputElement};
 use yew::prelude::*;
 use yew_icons::{Icon, IconId};
 
@@ -23,9 +26,13 @@ struct ChooseFolderArgs {
     title: String,
 }
 
+#[derive(Serialize)]
+struct CheckFolderArgs {
+    path: String,
+}
 // TODO:
-// validate before doing anything
 // implement project creation tauri function
+// *including error checking*
 
 #[function_component(ProjectWizard)]
 pub fn project_wizard(
@@ -34,8 +41,27 @@ pub fn project_wizard(
         closing_callback,
     }: &Props,
 ) -> Html {
+    let title = use_state(String::new);
     let location = use_state(|| default_location.clone());
+    let title_ref = use_node_ref();
+    let location_ref = use_node_ref();
+    let confirm_button_ref = use_node_ref();
     let is_hovered = use_state(|| false);
+    let is_data_valid = use_state(|| true);
+    let error_message = use_state(String::new);
+
+    let on_title_input = text_input_handler(title.clone());
+    let on_location_input = text_input_handler(location.clone());
+
+    {
+        let default_location = default_location.clone();
+        let location_ref = location_ref.clone();
+        use_effect_with((), move |()| {
+            if let Some(input) = location_ref.cast::<HtmlInputElement>() {
+                input.set_value(&default_location);
+            }
+        });
+    }
 
     let on_mouse_over = {
         let is_hovered = is_hovered.clone();
@@ -46,6 +72,78 @@ pub fn project_wizard(
         let is_hovered = is_hovered.clone();
         Callback::from(move |_| is_hovered.set(false))
     };
+
+    {
+        let is_data_valid = is_data_valid.clone();
+        let location = location.clone();
+        let error_message = error_message.clone();
+        use_effect_with((location.clone(), title.clone()), move |_| {
+            let is_data_valid = is_data_valid.clone();
+            let location = location.clone();
+            let error_message = error_message.clone();
+            if title.is_empty() {
+                is_data_valid.set(false);
+                error_message.set("Please enter a title".to_string());
+                return;
+            }
+            if location.is_empty() {
+                is_data_valid.set(false);
+                error_message.set("Please enter a location.".to_string());
+                return;
+            }
+            spawn_local(async move {
+                let is_data_valid = is_data_valid.clone();
+                let location = location.clone();
+                let error_message = error_message.clone();
+
+                let complete_path = PathBuf::from(&*location).join(&*title);
+
+                gloo_console::log!(format!(
+                    "Complete Path: {}",
+                    &complete_path
+                        .clone()
+                        .into_os_string()
+                        .into_string()
+                        .unwrap()
+                ));
+
+                let result = invoke(
+                    "can_create_path",
+                    serde_wasm_bindgen::to_value(&CheckFolderArgs {
+                        path: complete_path.into_os_string().into_string().unwrap(),
+                    })
+                    .unwrap(),
+                )
+                .await
+                .as_string()
+                .expect("Something went horribly wrong in validation");
+                //gloo_console::log!(format!("{}{}", &*location, &*is_data_valid));
+                let (is_valid, message) = match result.as_str() {
+                    "" => (true, ""),
+                    e => (false, e),
+                };
+
+                is_data_valid.set(is_valid);
+                error_message.set(message.to_string());
+            });
+        });
+    }
+
+    {
+        let confirm_button_ref = confirm_button_ref.clone();
+        use_effect_with(is_data_valid.clone(), move |_| {
+            if let Some(button) = confirm_button_ref.cast::<HtmlButtonElement>() {
+                gloo_console::log!(format!("Is valid: {}", *is_data_valid));
+                if *is_data_valid {
+                    let _ = button.style().set_property("opacity", "1");
+                    button.set_disabled(false);
+                } else {
+                    let _ = button.style().set_property("opacity", "0.5");
+                    button.set_disabled(true);
+                }
+            }
+        });
+    }
 
     let icon = if *is_hovered {
         html! {
@@ -67,8 +165,10 @@ pub fn project_wizard(
 
     let on_load = {
         let location = location.clone();
+        let location_ref = location_ref.clone();
         Callback::from(move |_: MouseEvent| {
             let location = location.clone();
+            let location_ref = location_ref.clone();
             spawn_local(async move {
                 let save_args = ChooseFolderArgs {
                     title: "Choose location".to_string(),
@@ -79,7 +179,12 @@ pub fn project_wizard(
                 let location_string = location_jsvalue.as_string();
                 match location_string {
                     None => (),
-                    Some(e) => location.set(e),
+                    Some(e) => {
+                        if let Some(input) = location_ref.cast::<HtmlInputElement>() {
+                            input.set_value(&e);
+                        }
+                        location.set(e);
+                    }
                 }
             });
         })
@@ -91,13 +196,18 @@ pub fn project_wizard(
             <br />
             <div class="font-semibold">{ "Name:" }</div>
             <div class="flex rounded-lg border-2 my-2 border-transparent hover:border-mauve">
-                <input class="outline-none w-full bg-crust text-text p-2 rounded-lg" />
+                <input
+                    oninput={on_title_input}
+                    ref={title_ref}
+                    class="outline-none w-full bg-crust text-text p-2 rounded-lg"
+                />
             </div>
             <br />
             <div class="font-semibold">{ "Location:" }</div>
             <div class="flex rounded-lg border-2 my-2 border-transparent hover:border-mauve">
                 <input
-                    value={(*location).clone()}
+                    oninput={on_location_input}
+                    ref={location_ref}
                     class="outline-none w-full bg-crust text-text p-2 rounded-tl-lg rounded-bl-lg"
                 />
                 <div
@@ -110,9 +220,19 @@ pub fn project_wizard(
                 </div>
             </div>
             <div id="footer" class="flex justify-end w-full pt-8">
+                <div class="text-text underline decoration-red break-words mr-auto">
+                    { (*error_message).clone() }
+                </div>
+                <button
+                    ref={confirm_button_ref}
+                    onclick={closing_callback}
+                    class={format!("rounded-lg px-2 py-1 ml-4 bg-mauve text-crust")}
+                >
+                    { "Confirm" }
+                </button>
                 <button
                     onclick={closing_callback}
-                    class={format!("rounded-lg px-2 py-1 ml-4 bg-red text-text")}
+                    class={format!("rounded-lg px-2 py-1 ml-4 bg-red text-crust")}
                 >
                     { "Close" }
                 </button>
@@ -133,3 +253,11 @@ pub fn project_wizard(
 //        </button>
 //    </>
 //)
+fn text_input_handler(value: UseStateHandle<String>) -> Callback<InputEvent> {
+    Callback::from(move |e: InputEvent| {
+        if let Some(input) = e.target_dyn_into::<HtmlInputElement>() {
+            let text = input.value();
+            value.set(text.clone());
+        }
+    })
+}
