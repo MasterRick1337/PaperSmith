a28cf4989e9bfc8c34b426fe52cb7bbb005323ba
DotDo <jannis.ehrnhofer@gmx.at>
2024-11-27 08:50:09 +0100

    fixed errors after rebase

--- src/app.rs
+++ src/app.rs
@@ -1,10 +1,8 @@
 use gloo::utils::document;
 use pulldown_cmark::{html, Options, Parser};
 use serde::{Serialize, Deserialize};
-use serde::{Serialize, Deserialize};
 use serde_wasm_bindgen::to_value;
 use sidebar::buttons::Button;
-use statistic::_CharCountProps::closing_callback;
 use statistic::_CharCountProps::closing_callback;
 use wasm_bindgen::prelude::wasm_bindgen;
 use wasm_bindgen::JsCast;
@@ -13,13 +11,12 @@ use wasm_bindgen_futures::spawn_local;
 use web_sys::HtmlDocument;
 use web_sys::HtmlElement;
 use web_sys::Node;
-use web_sys::Node;
 use yew::events::InputEvent;
 use yew::events::MouseEvent;
 use yew::prelude::*;
 use yew_icons::IconId;
 
-use serde::{Deserialize, Serialize};
+
 
 #[path = "menubar/zoom/zoom_handlers.rs"]
 mod zoom_edit_container_handlers;
@@ -28,8 +25,6 @@ use zoom_edit_container_handlers::ZoomControls;
 #[path = "toolbar/toolbar.rs"]
 mod toolbar;
 use toolbar::Toolbar;
-use yew_hooks::use_interval;
-use std::path::PathBuf;
 use yew_hooks::use_interval;
 use std::path::PathBuf;
 
@@ -49,9 +44,9 @@ use text_styling_handlers::TextStylingControls;
 mod statistic;
 use statistic::Statistics;
 
-#[path = "text_alignment_handlers.rs"]
-mod text_alignment_handlers;
-use text_alignment_handlers::TextAlignmentControls;
+//#[path = "text_alignment_handlers.rs"]
+//mod text_alignment_handlers;
+//use text_alignment_handlers::TextAlignmentControls;
 
 #[path = "sidebar/sidebar.rs"]
 mod sidebar;
@@ -87,21 +82,6 @@ pub struct FileWriteData {
 // pub struct StatisticProps {
 //     pub statistics: StatisticProp,
 // }
-#[derive(Properties, PartialEq)]
-pub struct WordCountProps {
-    pub pages_ref: NodeRef,
-}
-
-#[derive(Serialize, Deserialize)]
-pub struct FileWriteData {
-    pub path: String,
-    pub content: String
-}
-
-// #[derive(Properties, PartialEq)]
-// pub struct StatisticProps {
-//     pub statistics: StatisticProp,
-// }
 
 #[function_component(App)]
 pub fn app() -> Html {
@@ -116,106 +96,67 @@ pub fn app() -> Html {
     let text_alignment = use_state(|| "left".to_string());
     let sidebar = use_state(|| {
         html! {
-            <>
-                <div class="text-lg">{ "No Project Loaded" }</div>
-            </>
+            <div class="text-lg">{ "No Project Loaded" }</div>
         }
     });
     let modal = use_state(|| html!());
-
     let render_ref = use_node_ref();
 
     let on_text_input = text_input_handler(text_input_ref.clone(), render_ref.clone());
 
-
-    let project_path = project.as_ref().and_then(|proj| Some(proj.path.clone()));
+    let project_path = project.as_ref().map(|proj| proj.path.clone());
 
     let save = {
         let text_input_ref = text_input_ref.clone();
         let project_path = project_path.clone();
         let modal = modal.clone();
-    
-        let project_path = project_path.clone();
-        let modal = modal.clone();
-    
+
         Callback::from(move |_| {
             let text_input_ref = text_input_ref.clone();
             let project_path = project_path.clone();
             let modal = modal.clone();
-    
-            let project_path = project_path.clone();
-            let modal = modal.clone();
-    
+
             spawn_local(async move {
                 if let Some(input_element) = text_input_ref.cast::<HtmlElement>() {
                     let text = input_element.inner_text();
-    
-                    if let Some(mut path) = project_path.clone() {
+
+                    if let Some(mut path) = project_path {
                         path.push("Chapters");
                         path.push("Beginning");
                         path.push("Content.md");
-    
+
                         let write_data = FileWriteData {
                             path: path.to_string_lossy().to_string(),
-                            content: text
+                            content: text,
                         };
-    
-                        invoke("write_to_file", serde_wasm_bindgen::to_value(&write_data).unwrap()).await;
-    
+
+                        invoke(
+                            "write_to_file",
+                            serde_wasm_bindgen::to_value(&write_data).unwrap(),
+                        )
+                        .await;
+
                         modal.set(html! {
-                            // <Modal
-                            //     content={html! {
-                            //         <div>{ "Successfully saved" }</div>
-                            //     }}
-                            //     /*
-                            //     button_configs={
-                            //         vec![
-                            //             ModalButtonProps {
-                            //                 text: "Close".to_string(),
-                            //                 text_color: "white".to_string(),
-                            //                 bg_color: "green".to_string(),
-                            //                 callback: {
-                            //                     let modal = modal.clone();
-                            //                     Callback::from(move |_| modal.set(html!()))
-                            //                 }
-                            //             }
-                            //         ]
-                            //     }*/
-                            // />
-                        });
-    
-                    if let Some(mut path) = project_path.clone() {
-                        path.push("Chapters");
-                        path.push("Beginning");
-                        path.push("Content.md");
-    
-                        let write_data = FileWriteData {
-                            path: path.to_string_lossy().to_string(),
-                            content: text
-                        };
-    
-                        invoke("write_to_file", serde_wasm_bindgen::to_value(&write_data).unwrap()).await;
-    
-                        modal.set(html! {
-                            // <Modal
-                            //     content={html! {
-                            //         <div>{ "Successfully saved" }</div>
-                            //     }}
-                            //     /*
-                            //     button_configs={
-                            //         vec![
-                            //             ModalButtonProps {
-                            //                 text: "Close".to_string(),
-                            //                 text_color: "white".to_string(),
-                            //                 bg_color: "green".to_string(),
-                            //                 callback: {
-                            //                     let modal = modal.clone();
-                            //                     Callback::from(move |_| modal.set(html!()))
-                            //                 }
-                            //             }
-                            //         ]
-                            //     }*/
-                            // />
+                            /*
+                            <Modal
+                                content={html! {
+                                    <div>{ "Successfully saved" }</div>
+                                }}
+                                button_configs={
+                                    vec![
+                                        ModalButtonProps {
+                                            text: "Close".to_string(),
+                                            text_color: "white".to_string(),
+                                            bg_color: "green".to_string(),
+                                            callback: {
+                                                let modal = modal.clone();
+                                                Callback::from(move |_| modal.set(html!()))
+                                            }
+                                        }
+                                    ]
+                                }
+                            />
+                            */
                         });
                     }
                 }
@@ -230,57 +171,39 @@ pub fn app() -> Html {
             modal.set(html! {
                 <Modal
                     content={html! {
-                    <ProjectWizard
-
-                        closing_callback={
-                            let modal = modal.clone();
-                            Callback::from(move |_| modal.set(html!()))
-                        }
-                        project_ref={project.clone()}
-                    />
+                        <ProjectWizard
+                            closing_callback={
+                                let modal = modal.clone();
+                                Callback::from(move |_| modal.set(html!()))
+                            }
+                            project_ref={project.clone()}
+                        />
                     }}
                 />
             });
         })
     };
 
-    //let statistic_window = {
-    //    let modal = modal.clone();
-    //    let pages_ref = pages_ref.clone();
-    //    Callback::from(move |_| {
-    //        modal.set(html! {
-    //            <Modal
-    //            content={html!{<Statistics pages_ref={pages_ref.clone()}/>}}
-    //                button_configs={vec![
-    //                    ModalButtonProps {text:"Close".to_string(), text_color:"crust".to_string(), bg_color:"maroon".to_string(), callback: {
-    //                    let modal = modal.clone();
-    //                    Callback::from(move |_| modal.set(html!()))
-    //                    }}]}
-    //            />
-    //        });
-    //    })
-    //};
-
     {
         let sidebar = sidebar.clone();
         let project = project.clone();
         let text_input_ref = text_input_ref.clone();
         use_effect_with(project.clone(), move |_| {
-            if (*project).is_none() {
+            if project.is_none() {
                 sidebar.set(html! {
                     <div class="cursor-default select-none text-lg">{ "No Project Loaded" }</div>
                 });
             } else {
                 sidebar.set(
-                    html! { <SideBar project={project.clone()} input_ref={text_input_ref} /> },
+                    html! { <SideBar project={project.clone()} input_ref={text_input_ref.clone()} /> },
                 );
             }
         });
-    };
+    }
 
     let on_load = {
-        //let project = project.clone();
-        Callback::from(move |_: MouseEvent| {
+        let project = project.clone();
+        Callback::from(move |_| {
             let project = project.clone();
             spawn_local(async move {
                 let project_jsvalue = invoke("get_project", JsValue::null()).await;
@@ -293,22 +216,15 @@ pub fn app() -> Html {
         })
     };
 
-    let on_undo = Callback::from(move |_: MouseEvent| {
+    let on_undo = Callback::from(move |_| {
         let html_doc: HtmlDocument = document().dyn_into().unwrap();
         html_doc.exec_command("undo").unwrap();
     });
 
-    let on_redo = Callback::from(move |_: MouseEvent| {
+    let on_redo = Callback::from(move |_| {
         let html_doc: HtmlDocument = document().dyn_into().unwrap();
         html_doc.exec_command("redo").unwrap();
     });
-
-    //let print_project = {
-    //    Callback::from(move |_| {
-    //        let project = project.clone();
-    //        gloo_console::log!(format!("{}", project.as_ref().unwrap()));
-    //    })
-    //};
 
     html! {
         <div>
@@ -316,9 +232,7 @@ pub fn app() -> Html {
             <div class="modal-wrapper">{ (*modal).clone() }</div>
             <style id="dynamic-style" />
             <Toolbar />
-            <div
-                class="menubar bg-crust border-solid border-t-[2px] border-x-0 border-b-0 border-text"
-            >
+            <div class="menubar bg-crust border-solid border-t-[2px] border-x-0 border-b-0 border-text">
                 <Button callback={open_modal} icon={IconId::LucideFilePlus} size=1.5 />
                 <Button callback={on_load} icon={IconId::LucideFolderOpen} size=1.5 />
                 <Button callback={save} icon={IconId::LucideSave} size=1.5 />
@@ -370,32 +284,22 @@ pub fn app() -> Html {
                     />
                 </div>
             </div>
-            <div
-                class="bottombar bg-crust border-solid border-t-[2px] border-x-0 border-b-0 border-text"
-            >
+            <div class="bottombar bg-crust border-solid border-t-[2px] border-x-0 border-b-0 border-text">
                 <div class="bottombar-left">
-                    //<Statistics
-                    //closing_callback={
-                    //    let modal = modal.clone();
-                    //    Callback::from(move |_| modal.set(html!()))
-                    //}
-                    //pages_ref={pages_ref.clone()}
-                    // />
-                    //<div>{format!("{}, {} Words; Characters: {}, {} without spaces, {:.2} wpm", *session_time, *word_count, *char_count,*char_count_no_spaces, calculated_wpm)}</div>
-                    //{format!("{}", props.statistics.char_count)}
                     <Statistics
                         closing_callback={
                             let modal = modal.clone();
                             Callback::from(move |_| modal.set(html!()))
-                    }
-                    pages_ref={pages_ref.clone()}
+                        }
+                        pages_ref={pages_ref.clone()}
                     />
                 </div>
                 <div class="bottombar-right" />
             </div>
-        </>
+        </div>
     }
 }
+
 
 fn text_input_handler(text_input_ref: NodeRef, render_ref: NodeRef) -> Callback<InputEvent> {
     Callback::from(move |_| {
